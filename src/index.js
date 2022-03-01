2a7326df03fee5a32020a924e2b06167a53b467a
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-03-01 12:04:39 +0900

    converted to liferay remote app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App";
 import "./index.css";
-import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class WebComponent extends HTMLElement {
+    connectedCallback() {
+        ReactDOM.render(<App />, this)
+    }
+}
+
+const ELEMENT_ID = "calculator-app";
+
+if (!customElements.get(ELEMENT_ID)) {
+    customElements.define(ELEMENT_ID, WebComponent)
+}
